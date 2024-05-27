4435560653cd22b6f883e9d542eaa54e087e7078
Lorelle Reid <lorelle.reid@gmail.com>
2024-05-27 17:59:51 -0400

    ➕ ADDING: vscode

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-tabs': 'off', // disallow tab characters entirely
+    indent: "tab",
+    'comma-dangle': ["error", "only-multiline"],
+  },
+};
